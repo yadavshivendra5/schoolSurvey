c54a00a120f8fdfafa389e2f386bf5536c32b3d4
Shivendra Yadav <yadavshivendra5@gmail.com>
2026-04-05 14:44:35 +0530

    port

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", schoolRoutes);
-
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
